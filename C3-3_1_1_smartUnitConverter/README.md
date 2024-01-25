be3f7d2e0c8d3ac306f3bb0971a58738a0675ffa
Jan Schulze <58750565+ThatJanGuy@users.noreply.github.com>
2024-01-25 17:58:41 +0100

    Adds solution for 3.1.1 "smartUnitConverter".

---
+++ C3-3_1_1_smartUnitConverter/README.md
@@ -0,0 +1,30 @@
+# Task
+
+## Example
+
+The following entry indicates that there are four values to be converted. The first is 10 meters, which, when converted, gives approximately 32.808 feet. The second is 1245.243 grams, or about 2.745761 pounds, the third is 37.2 degrees Celsius, or 98.96 degrees Farenheit, and the fourth is 23 grams, or 0.050715 pounds.
+
+### Input
+
+```
+4
+10 m
+1245.243 g
+37.2 c
+23 g
+```
+
+### Output
+
+```
+32.808000 ft
+2.745761 lbs
+98.960000 f
+0.050715 lbs
+```
+
+*Warning:* You will be graded on your output, so do not include any print statements that prompt a user for input.
+
+## Note
+
+You may submit your solution as many times as you wish. Only your highest score will be kept.
---
+++ C3-3_1_1_smartUnitConverter/smartUnitConverter.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+// Prototypes
+double metersToFeet(double);
+double gramsToPounds(double);
+double celsiusToFahrenheit(double);
+
+
+// MAIN
+int main(void){
+    int noOfConversions = 0;
+    int maxNoOfConversions = 10;
+    double inOutValues[maxNoOfConversions];
+    char inOutUnits[maxNoOfConversions];
+
+    // Iitialises the in-out arrays to a clean state.
+    for (int i = 0; i < maxNoOfConversions; i++){
+        inOutValues[i] = 0;
+        inOutUnits[i] = '\0';
+    }
+
+    scanf("%d", &noOfConversions);
+
+    for (int i = 0; i < noOfConversions; i++){
+        scanf("%lf %c", &inOutValues[i], &inOutUnits[i]);
+    }
+
+    for (int i = 0; i < noOfConversions; i++){
+        if (inOutUnits[i] == 'm') {
+            printf("%lf ft\n", metersToFeet(inOutValues[i]));
+        } else if (inOutUnits[i] == 'g') {
+            printf("%lf lbs\n", gramsToPounds(inOutValues[i]));
+        } else if (inOutUnits[i] == 'c') {
+            printf("%lf f\n", celsiusToFahrenheit(inOutValues[i]));
+        }
+
+        inOutValues[i] = 0;
+        inOutUnits[i] = '\0';
+    }
+
+    return (0);
+}
+
+//Functions
+double metersToFeet(double input){
+    return (input * 3.28080);
+}
+
+double gramsToPounds(double input){
+    return (input * 0.002205);
+}
+
+double celsiusToFahrenheit(double input){
+    return ((input * 9 / 5) + 32);
+}
