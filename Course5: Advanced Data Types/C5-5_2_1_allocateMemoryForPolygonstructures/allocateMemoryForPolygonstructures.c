a360625c76c5fca6a6ef1be6b963555efc08a5a1
Jan Schulze <58750565+ThatJanGuy@users.noreply.github.com>
2024-02-24 18:53:04 +0100

    Adds solution to 5.2.1.

--- Course5: Advanced Data Types/C5-5_2_1_allocateMemoryForPolygonstructures/allocateMemoryForPolygonstructures.c
+++ Course5: Advanced Data Types/C5-5_2_1_allocateMemoryForPolygonstructures/allocateMemoryForPolygonstructures.c
@@ -13,7 +13,16 @@ void initializePoly(struct point *, int);
 int main(void) {
     
     // Fill in your main function here
+    int vertices = 0;
+    struct point * polygonPtr;
 
+    scanf("%d", &vertices);
+    polygonPtr = (struct point *) malloc(vertices * sizeof(struct point));
+    initializePoly(polygonPtr, vertices);
+    printPoly(polygonPtr, vertices);
+    free(polygonPtr);
+
+    return 0;
 }
 
 void printPoint(struct point pt) {
@@ -28,3 +37,9 @@ void printPoly(struct point *ptr, int N) {
 }
 
 // Write your initializePoly() function here
+void initializePoly(struct point * polyPtr, int vertices){
+    for (int i = 0; i < vertices; i++){
+        polyPtr[i].x = -i;
+        polyPtr[i].y = i*i;
+    }
+}
