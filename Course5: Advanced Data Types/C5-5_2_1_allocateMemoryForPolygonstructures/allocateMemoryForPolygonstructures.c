6f8e9db152372e7b36921dcf6d83c77555c93ede
Jan Schulze <58750565+ThatJanGuy@users.noreply.github.com>
2024-02-24 18:08:06 +0100

    Prepares folder and task description for 5.2.1.

---
+++ Course5: Advanced Data Types/C5-5_2_1_allocateMemoryForPolygonstructures/allocateMemoryForPolygonstructures.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct point{
+	int x;
+	int y;
+};
+
+void printPoint(struct point);
+void printPoly(struct point *, int);
+void initializePoly(struct point *, int);
+
+int main(void) {
+    
+    // Fill in your main function here
+
+}
+
+void printPoint(struct point pt) {
+    printf("(%d, %d)\n", pt.x, pt.y);
+}
+
+void printPoly(struct point *ptr, int N) {
+    int i;
+    for (i=0; i<N; i++) {
+        printPoint(ptr[i]);
+    }
+}
+
+// Write your initializePoly() function here
